2ba288a5da7e694f772a1775bd68bd8d7337baee
oierur05 <103904122+oierur05@users.noreply.github.com>
2024-11-12 16:06:39 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# Función para sumar dos números
+def suma(num1, num2):
+    return num1 + num2
+
+# Variables con dos números
+numero1 = 5
+numero2 = 8
+
+# Imprimir la suma de los dos números usando la función
+print("La suma de", numero1, "y", numero2, "es:", suma(numero1, numero2))
